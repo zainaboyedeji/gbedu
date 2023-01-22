0d530d0b0665af216c6735dc757c12620493595c
Zainab Oyedeji <ololadeoselu@yahoo.com>
2023-01-22 15:58:09 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import PlayerWidget from "./components/PlayerWidget";
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -16,6 +16,7 @@ export default function App() {
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
+        <PlayerWidget/>
       </SafeAreaProvider>
     );
   }
---
+++ screens/AlbumScreen.tsx
@@ -0,0 +1,26 @@
+import React, {useEffect} from 'react';
+import {View,Text, FlatList} from 'react-native';
+import {useRoute} from '@react-navigation/native';
+import SongListItem from "../components/SongListItem"
+import albumDetails from "../data/albumDetails";
+import AlbumHeader from "../components/AlbumHeader";
+
+const AlbumScreen = () => {
+	const route = useRoute();
+
+	useEffect(() => {
+		console.log(route);
+	},[])
+	return(
+	<View>
+		<FlatList
+		data={albumDetails.songs}
+		renderItem={({item}) => <SongListItem song={item}/>}
+		keyExtractor={(item) => item.id}
+		ListHeaderComponent={() => <AlbumHeader album={albumDetails} />}
+		/>
+	</View>
+)
+}
+
+export default AlbumScreen;
